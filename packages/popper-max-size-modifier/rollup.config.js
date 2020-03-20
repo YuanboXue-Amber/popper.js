a22d50f676f3c87c7be6b4a8a94b80257b9d5e6c
atomiks <cc.glows@gmail.com>
2020-03-20 22:41:03 +1100

    chore: delete nested rollup config

--- packages/popper-max-size-modifier/rollup.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-import createRollupConfig from '../build-utils/createRollupConfig';
-
-export default createRollupConfig({
-  modifierName: process.env.MODIFIER_NAME,
-  packageName: process.env.PACKAGE_NAME
-});
