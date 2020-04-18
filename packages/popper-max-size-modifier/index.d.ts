febb1dc0db08631ba8aa31dc0b72f11c5436040d
atomiks <cc.glows@gmail.com>
2020-04-18 21:45:02 +1000

    fix(types): Modifier type

--- packages/popper-max-size-modifier/index.d.ts
+++ packages/popper-max-size-modifier/index.d.ts
@@ -1,5 +1,22 @@
-import {Modifier} from '@popperjs/core';
+import {
+  Modifier,
+  Placement,
+  Boundary,
+  RootBoundary,
+  Context,
+  Padding
+} from '@popperjs/core';
 
-declare const maxSize: Modifier<{}>;
+// TODO: import from @popperjs/core directly once this can be exported
+type Options = {
+  placement: Placement;
+  boundary: Boundary;
+  rootBoundary: RootBoundary;
+  elementContext: Context;
+  altBoundary: boolean;
+  padding: Padding;
+};
+
+declare const maxSize: Modifier<'maxSize', Options>;
 
 export default maxSize;
